69b2421d5439bdd7d86dbdf5e5d34d9e4c377b2a
sohyun <shparkso162@gmail.com>
2021-07-29 20:16:32 +0900

    connect FIREBASE

---
+++ app/src/main/java/dduwcom/mobile/simple_sns/MainActivity.java
@@ -0,0 +1,14 @@
+package dduwcom.mobile.simple_sns;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
